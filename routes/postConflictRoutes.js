bb6da9674bc64646fa800ccd2aea672ac1eb8454
arjun7095 <boyapatidurga8@gmail.com>
2026-02-28 17:43:14 +0530

    nckjdnk

--- routes/postConflictRoutes.js
+++ routes/postConflictRoutes.js
@@ -10,7 +10,7 @@ router.put('/:sessionId/step1', postConflictController.updateStep1);
 router.put('/:sessionId/step2', postConflictController.updateStep2);
 router.put('/:sessionId/step3', postConflictController.updateStep3);
 router.put('/:sessionId/complete', postConflictController.completeSession);
-router.get('/', postConflictController.getSessions);
+router.get('/:sessionId', postConflictController.getSessions);
 // router.get('/:sessionId', postConflictController.getSession);
 router.get('/:sessionId/resume', postConflictController.resumeSession);
 
