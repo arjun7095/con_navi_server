351108b139f78e8611c53f2f331efc298d308dd2
arjun7095 <boyapatidurga8@gmail.com>
2026-02-28 18:06:13 +0530

    final confirmation of post con

--- routes/postConflictRoutes.js
+++ routes/postConflictRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const postConflictController = require('../controllers/postConflictController');
-// const protect = require('../middleware/auth');  // protect all
+const protect = require('../middleware/auth');  // protect all
 
-// router.use();
+router.use(protect);
 
 router.post('/', postConflictController.createSession);
 router.put('/:sessionId/step1', postConflictController.updateStep1);
