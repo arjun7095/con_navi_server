9c54c0da8170adab5b9251c93d218db73d07a7b9
arjun7095 <boyapatidurga8@gmail.com>
2026-03-05 01:00:56 +0530

    fcm token testing

--- config/firebase.js
+++ config/firebase.js
@@ -4,3 +4,13 @@ const serviceAccount = require('../serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+admin.messaging().send({
+  token: "ekeCbTJASGa0KQ1W7kMvVd:APA91bFZ6uFo0Oo_KGpwNEatDEbhNrbG_20hemvuYEMGwjB4cOZsDEcOVPsTCyO65oS8vj1kPJOPtGKnaGXQt-ZxlxEgsQ9aLLdf7qq_8RytJZrj3t-n0PM",
+  notification: {
+    title: "Test",
+    body: "Working"
+  }
+})
+.then(console.log)
+.catch(console.error);
